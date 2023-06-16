6e2957521fe9e9bebd0dfa7d49bd359c56942d9e
Punyaartwork <49674661+Punyaartwork@users.noreply.github.com>
2023-06-17 01:09:47 +0700

    add

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/donations', function () {
+    return view('donations');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
